fb5b9b75f6710221689967d7ce0edad24a933d0f
atomd <dwb319@gmail.com>
2012-10-26 23:54:52 +0800

    Update publish script

--- jekyll-publish.sh
+++ jekyll-publish.sh
@@ -4,6 +4,8 @@
 cd $(dirname "$0")
 
 git checkout -q master
+echo ''
+
 [ "$(git status -s)" ] \
     && echo "[ERROR] Not all files are committed. Could not publish the new version of blog." \
     && exit 1
@@ -23,5 +25,8 @@ cp -r _site/. .
 rm -r _site
 git add -A
 git commit -m "Auto generated at $(date)"
+
+git checkout -q master
+echo ''
 
 git push --all
