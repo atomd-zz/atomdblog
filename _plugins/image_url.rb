faab220d5ecf181bdcb57319c5b78894e8347c2c
atomd <dwb319@gmail.com>
2012-10-30 00:26:40 +0800

    Modify the permalink

--- _plugins/image_url.rb
+++ _plugins/image_url.rb
@@ -1,6 +1,9 @@
 module Jekyll
   class ImageUrl < Liquid::Tag
-    MATCHER = /^\/(\w+)\/(\d+)\/(\d+)\/(\d+)\/(.*)\/$/
+
+    MATCHER_URL = /^\/(\w+)\/(.*)\/$/
+
+    MATCHER_DATE = /(\d+)-(\d+)-(\d+)\s.+/
 
     def initialize(tag_name, img, tokens)
       super
@@ -9,8 +12,12 @@ module Jekyll
 
     def render(context)
       page_url = context.environments.first["page"]["url"]
-      who, cares, year, month, day, title = *page_url.match(MATCHER)
+      page_date = context.environments.first["page"]["date"]
+
+      _, _, title = *page_url.match(MATCHER_URL)
+      _, year, month, day = *page_date.to_s.match(MATCHER_DATE)
       return "/images/#{year}-#{month}-#{day}-#{title}/#{@orig_img}"
+
     end
   end
 end
