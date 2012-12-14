0d8a1e272eba733330ac8e6ea689ad796ea2b70d
atomd <dwb319@gmail.com>
2012-12-15 03:34:15 +0800

    fix plugin

--- _plugins/image_url.rb
+++ _plugins/image_url.rb
@@ -3,8 +3,6 @@ module Jekyll
 
     MATCHER_URL = /^\/(\w+)\/(.*)\/$/
 
-    MATCHER_DATE = /(\d+)-(\d+)-(\d+)\s.+/
-
     def initialize(tag_name, img, tokens)
       super
       @orig_img = img.strip
@@ -12,11 +10,8 @@ module Jekyll
 
     def render(context)
       page_url = context.environments.first["page"]["url"]
-      page_date = context.environments.first["page"]["date"]
-
       _, _, title = *page_url.match(MATCHER_URL)
-      _, year, month, day = *page_date.to_s.match(MATCHER_DATE)
-      return "/images/#{year}-#{month}-#{day}-#{title}/#{@orig_img}"
+      return "/images/#{title}/#{@orig_img}"
 
     end
   end
