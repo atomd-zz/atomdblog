d63cfa2ff9a45ac606285cc82ae5c55c47aef8a5
atomd <dwb319@gmail.com>
2013-01-19 22:37:17 +0800

    Fix warnings

--- _plugins/post_url.rb
+++ _plugins/post_url.rb
@@ -1,12 +1,12 @@
 module Jekyll
 
   class PostComparer
-    MATCHER = /^(.+\/)*(\d+-\d+-\d+)-(.*)$/
+    _MATCHER = /^(.+\/)*(\d+-\d+-\d+)-(.*)$/
 
     attr_accessor :date, :slug
 
     def initialize(name)
-      who, cares, date, slug = *name.match(MATCHER)
+      who, cares, date, slug = *name.match(_MATCHER)
       @slug = slug
       @date = Time.parse(date)
     end
